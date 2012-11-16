322a0ff258b712182b1f406a9119d9ae924e94a9
Adam Brenecki <adam@brenecki.id.au>
2012-11-17 09:56:15 +1030

    Ignore errors on call to Pandoc so that yorick installs properly from a sdist tarball

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ import subprocess
 
 def get_long_desc():
 	"""Use Pandoc to convert the readme to ReST for the PyPI."""
-	return subprocess.check_output(['pandoc', '-f', 'markdown', '-t', 'rst', 'README.md'])
+	try:
+		return subprocess.check_output(['pandoc', '-f', 'markdown', '-t', 'rst', 'README.md'])
+	except:
+		print "WARNING: The long readme wasn't converted properly"
 
 setup(name='yorick',
 	version='0.1pre',
